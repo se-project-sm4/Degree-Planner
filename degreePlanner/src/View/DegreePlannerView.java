2b3016aee12b95fe2025edcca47d8f309d347479
mm812 <53228050+mm812@users.noreply.github.com>
2019-07-24 17:14:03 -0500

    Update DegreePlannerView.java
    added the registration screen

--- degreePlanner/src/View/DegreePlannerView.java
+++ degreePlanner/src/View/DegreePlannerView.java
@@ -36,14 +36,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 */
-public class DegreePlannerView extends JFrame {
+public class DegreePlannerView extends JFrame implements ActionListener{
 
     public static void main(String[] args) {
         
         DegreePlannerView frame = new DegreePlannerView();
     }
+    String adminUser = "user";
+    String adminPass = "pass";
 
     JButton login = new JButton("Login");
+    JButton register = new JButton("Register");
     JPanel panel = new JPanel();
     JTextField username = new JTextField(15);
     JPasswordField pass = new JPasswordField(15);
@@ -56,13 +59,15 @@ public class DegreePlannerView extends JFrame {
         setLocation(500,280);
         panel.setLayout (null);
 
-        username.setBounds(70,35,150,20);
-        pass.setBounds(70,75,150,20);
+        username.setBounds(50,35,200,20);
+        pass.setBounds(50,75,200,20);
         userLab.setBounds(70,15,150,20);
         passLab.setBounds(70,55,150,20);
-        login.setBounds(105,110,80,20);
+        login.setBounds(65,110,80,20);
+        register.setBounds(160,110,80,20);
 
         panel.add(login);
+        panel.add(register);
         panel.add(userLab);
         panel.add(username);
         panel.add(passLab);
@@ -71,28 +76,93 @@ public class DegreePlannerView extends JFrame {
         getContentPane().add(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        actionlogin();
+        login.addActionListener(this);
+        register.addActionListener(this);
+
     }
+    public void actionPerformed(ActionEvent e){
 
-    public void actionlogin(){
-        login.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                String puname = username.getText();
-                String ppaswd = pass.getText();
-                if(puname.equals("user") && ppaswd.equals("pass")) {
-                    DegreeWindow regFace =new DegreeWindow();
-                    regFace.setVisible(true);
-                    dispose();
-                } else {
+        if(e.getSource()==login){
+            String puname = username.getText();
+            String ppaswd = pass.getText();
+            if(puname.equals(adminUser) && ppaswd.equals(adminPass)) {
+                DegreeWindow regFace =new DegreeWindow();
+                regFace.setVisible(true);
+                dispose();
+            } else {
 
-                    JOptionPane.showMessageDialog(null,"Wrong Password / Username");
-                    username.setText("");
-                    pass.setText("");
-                    username.requestFocus();
-                }
-
+                JOptionPane.showMessageDialog(null,"Wrong Password / Username");
+                username.setText("");
+                pass.setText("");
+                username.requestFocus();
             }
+
+        }
+        else if(e.getSource()==register){
+            Actionregister();
+        }
+
+    }
+    public void Actionregister(){
+
+        JButton register2 = new JButton("Register");
+        JPanel panel2 = new JPanel();
+        JTextField newUser = new JTextField(15);
+        JPasswordField newPass = new JPasswordField(15);
+        JLabel userLab2 = new JLabel("Enter new username:");
+        JLabel passLab2 = new JLabel("Enter new password:");
+
+        JFrame frame=new JFrame();
+        frame.setLocationRelativeTo(null);
+
+            frame.setSize(300, 200);
+            setLocation(500, 280);
+            panel.setLayout(null);
+
+            newUser.setBounds(50, 35, 200, 20);
+            newPass.setBounds(50, 75, 200, 20);
+            userLab2.setBounds(70, 15, 150, 20);
+            passLab2.setBounds(70, 55, 150, 20);
+            register2.setBounds(65, 110, 80, 20);
+
+            panel2.add(userLab2);
+            panel2.add(newUser);
+            panel2.add(passLab2);
+            panel2.add(newPass);
+            panel2.add(register2);
+
+            getContentPane().add(panel2);
+
+            frame.setContentPane(panel2);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400,200);
+            frame.setVisible(true);
+            register2.addActionListener(
+        new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e) {
+                {
+                    String newPuname = newUser.getText();
+                    String newPpaswd = newPass.getText();
+                    if(newPuname.equals(adminUser) && newPpaswd.equals(adminPass)) {
+
+                        JOptionPane.showMessageDialog(null,"Error: already taken");
+                        username.setText("");
+                        pass.setText("");
+                        username.requestFocus();
+                    } else {
+
+                        JOptionPane.showMessageDialog(null,"Successfully added, logging in");
+
+                        DegreeWindow regFace = new DegreeWindow();
+                        regFace.setVisible(true);
+                        dispose();
+
+                    }
+                    }
+                }
         });
     }
+
 }
 //>>>>>>> 3990c57211b4dd7214000ae9e5a5105e843a2607
