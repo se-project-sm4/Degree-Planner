e3cfdf5123bb915794ab2ec1e7233cc170b9921f
jordan-matthews-98 <45404939+jordan-matthews-98@users.noreply.github.com>
2019-07-27 21:37:36 -0500

    made use of LOGIN and REGISTER Strings

--- degreePlanner/src/View/DegreePlannerView.java
+++ degreePlanner/src/View/DegreePlannerView.java
@@ -5,14 +5,46 @@ import java.awt.event.*;
 import Controller.DegreePlannerController;
 import Model.DegreePlannerModel;
 import Model.ModelEvent;
+import View.LoginView.Handler;
 
 public class DegreePlannerView extends JFrameView
 {
-	//create operation items here
+	public static final String CREATE_PLAN = "Create Plan";
+	public static final String CLEAR_PLAN = "Clear Plan";
+	public static final String ADD_COURSE = "Add Course";
+	public static final String REMOVE_COURSE = "Remove Course";
+	public static final String ADD_MAJOR = "Add Major";
+	public static final String REMOVE_MAJOR = "Remove Major";
+	public static final String ADD_MINOR = "Add Minor";
+	public static final String REMOVE_MINOR = "Remove Minor";
+	public static final String LOGOUT = "Log Out";
     public DegreePlannerView(DegreePlannerModel model, DegreePlannerController controller){
 		super(model, controller); 
-		//create non-operation items here
-		//add items to view
+		JPanel buttonPanel = new JPanel();
+		Handler handler = new Handler();
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(handler);
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(handler);
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(handler);
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(handler);
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(handler);
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(handler);
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(handler);
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(handler);
+		JButton loginButton = new JButton("Log Out");
+		loginButton.addActionListener(handler);
+		buttonPanel.setLayout(new GridLayout(1, 2));
+		this.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+		buttonPanel.add(loginButton, null);
+		buttonPanel.add(registerButton, null);
+		pack();
     }
 
 	public void modelChanged(ModelEvent event) {
