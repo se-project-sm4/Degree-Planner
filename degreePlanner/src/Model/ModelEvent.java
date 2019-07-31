4aa7056a2cad2ed7ffb766a818c71f5b954949da
jordan-matthews-98 <45404939+jordan-matthews-98@users.noreply.github.com>
2019-07-30 19:44:24 -0500

    added List<List<String>> semesters

--- degreePlanner/src/Model/ModelEvent.java
+++ degreePlanner/src/Model/ModelEvent.java
@@ -1,16 +1,24 @@
 package Model;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModelEvent extends ActionEvent {
 	private static final long serialVersionUID = 1L;
 	private int type;
+	private List<List<String>> semesters = new ArrayList<List<String>>();
 	
-	public ModelEvent(Object obj, int id, String message, int t){
+	public ModelEvent(Object obj, int id, String message, int t, List<List<String>> s){
 		super(obj, id, message);
 		type = t;
+		semesters = s;
 	}
 	
 	public int getType(){
 		return type;
+	}
+	
+	public List<List<String>> getSemesters(){
+		return semesters;
 	}
 }
